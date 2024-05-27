56f892bffc40dd34f82a1ba14bce72b4252c2807
Ben Parizek <ben.parizek@gmail.com>
2024-05-27 18:38:38 -0400

    Adds Form Element and Submission Element migration

---
+++ src/forms/migrations/m240527_000000_content_refactor.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace BarrelStrength\Sprout\forms\migrations;
+
+use BarrelStrength\Sprout\forms\FormsModule;
+use BarrelStrength\Sprout\forms\formtypes\FormTypeHelper;
+use craft\db\Query;
+use craft\migrations\BaseContentRefactorMigration;
+
+class m240527_000000_content_refactor extends BaseContentRefactorMigration
+{
+    public const FORMS_TABLE = '{{%sprout_forms}}';
+
+    public const FORM_SUBMISSIONS_TABLE = '{{%sprout_form_submissions}}';
+
+    public function safeUp(): void
+    {
+        $formTypes = FormTypeHelper::getFormTypes();
+
+        foreach ($formTypes as $formType) {
+            $this->updateElements(
+                (new Query())
+                    ->from(self::FORMS_TABLE)
+                    ->where(['formTypeUid' => $formType->uid]),
+                $formType->getFieldLayout(),
+            );
+        }
+
+        $forms = FormsModule::getInstance()->forms->getAllForms();
+
+        foreach ($forms as $form) {
+            $this->updateElements(
+                (new Query())->from(self::FORM_SUBMISSIONS_TABLE),
+                $form->getSubmissionFieldLayout(),
+            );
+        }
+    }
+
+    public function safeDown(): bool
+    {
+        echo self::class . " cannot be reverted.\n";
+
+        return false;
+    }
+}
